0736bf7a929605f419a3fd355d649fbc2253e424
Atanas Atanasov <atanas.atanasov@mobisystems.com>
2022-12-24 14:34:00 +0200

    clean main

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from src.download_from_server import DownloadFiles
-from src.get_urls import GetContent
 from src.main_window import UserInterface
 
 
 def main():
-    # gb = GetBooks()
-    # gb.get_urls()
-    # dl = DownloadBooks()
-    # dl.download()
     mw = UserInterface()
     mw.run_main_window()
 
